34b1390cf48f04402310f74512339bef3a0725aa
matt <flowingfirefly@foxmail.com>
2023-04-25 23:33:18 +0800

    humidity uuid

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,16 @@
 #include <Arduino.h>
-#include <BLEDevice.h>
-#include <BLEUtils.h>
-#include <BLEScan.h>
 #include <BLEAdvertisedDevice.h>
+#include <BLEDevice.h>
+#include <BLEScan.h>
 #include <BLEServer.h>
+#include <BLEUtils.h>
 
-#include <Wire.h>
 #include <AHTxx.h>
+#include <Wire.h>
 
 #define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+#define HUMIDiTY_CHARACTERISTIC_UUID "32db52a6-5c87-407b-8bc0-c4170df277cd"
 
 #define GPIO4 4
 #define GPIO5 5
@@ -23,29 +24,34 @@ BLECharacteristic *pCharacteristic = nullptr;
 
 void printStatus() {
   switch (aht10.getStatus()) {
-    case AHTXX_NO_ERROR:
-      Serial.println(F("no error"));
-      break;
+  case AHTXX_NO_ERROR:
+    Serial.println(F("no error"));
+    break;
 
-    case AHTXX_BUSY_ERROR:
-      Serial.println(F("sensor busy, increase polling time"));
-      break;
+  case AHTXX_BUSY_ERROR:
+    Serial.println(F("sensor busy, increase polling time"));
+    break;
 
-    case AHTXX_ACK_ERROR:
-      Serial.println(F("sensor didn't return ACK, not connected, broken, long wires (reduce speed), bus locked by slave (increase stretch limit)"));
-      break;
+  case AHTXX_ACK_ERROR:
+    Serial.println(
+        F("sensor didn't return ACK, not connected, broken, long wires (reduce "
+          "speed), bus locked by slave (increase stretch limit)"));
+    break;
 
-    case AHTXX_DATA_ERROR:
-      Serial.println(F("received data smaller than expected, not connected, broken, long wires (reduce speed), bus locked by slave (increase stretch limit)"));
-      break;
+  case AHTXX_DATA_ERROR:
+    Serial.println(F(
+        "received data smaller than expected, not connected, broken, long "
+        "wires (reduce speed), bus locked by slave (increase stretch limit)"));
+    break;
 
-    case AHTXX_CRC8_ERROR:
-      Serial.println(F("computed CRC8 not match received CRC8, this feature supported only by AHT2x sensors"));
-      break;
+  case AHTXX_CRC8_ERROR:
+    Serial.println(F("computed CRC8 not match received CRC8, this feature "
+                     "supported only by AHT2x sensors"));
+    break;
 
-    default:
-      Serial.println(F("unknown status"));
-      break;
+  default:
+    Serial.println(F("unknown status"));
+    break;
   }
 }
 
@@ -59,25 +65,28 @@ void setup() {
   BLEServer *pServer = BLEDevice::createServer();
   BLEService *pService = pServer->createService(SERVICE_UUID);
   pCharacteristic = pService->createCharacteristic(
-    CHARACTERISTIC_UUID,
-    BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE);
+      CHARACTERISTIC_UUID,
+      BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE);
 
   pCharacteristic->setValue("Hello World says Neil");
   pService->start();
-  // BLEAdvertising *pAdvertising = pServer->getAdvertising();  // this still is working for backward compatibility
+  // BLEAdvertising *pAdvertising = pServer->getAdvertising();  // this still is
+  // working for backward compatibility
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
   BLEAdvertisementData advertiseData;
   advertiseData.setServiceData(BLEUUID(CHARACTERISTIC_UUID), "foo");
   pAdvertising->setAdvertisementData(advertiseData);
   pAdvertising->addServiceUUID(SERVICE_UUID);
   pAdvertising->setScanResponse(true);
-  pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
+  pAdvertising->setMinPreferred(
+      0x06); // functions that help with iPhone connections issue
   pAdvertising->setMinPreferred(0x12);
   BLEDevice::startAdvertising();
   Serial.println("Characteristic defined! Now you can read it in your phone!");
 
   while (!aht10.begin(GPIO4, GPIO5)) {
-    Serial.println(F("AHT1x not connected or fail to load calibration coefficient"));
+    Serial.println(
+        F("AHT1x not connected or fail to load calibration coefficient"));
     delay(5000);
   }
 
